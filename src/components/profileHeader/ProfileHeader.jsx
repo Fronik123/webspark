f41a31d8f6166b73a890c1d7108c39238515b28b
Mykola Korostelov <askolya23@gmail.com>
2026-06-08 22:21:46 +0300

    Add background images and update styles for ProfileHeader and App components

--- src/components/profileHeader/ProfileHeader.jsx
+++ src/components/profileHeader/ProfileHeader.jsx
@@ -1,10 +1,21 @@
 import { profile } from "../../data/profile";
 import styles from "./ProfileHeader.module.css";
+import background from "../../assets/background.png";
+import logo from "../../assets/logo.png";
 
 export default function ProfileHeader() {
   const { username, startDate, stats } = profile;
   return (
     <header className={styles.header}>
+      <img
+        src={background}
+        alt=""
+        className={styles.background}
+        aria-hidden="true"
+      />
+
+      <img src={logo} alt="" className={styles.logo} aria-hidden="true" />
+
       <div className={styles.headerContent}>
         <div className={styles.top}>
           <h1 className={styles.username}>{username}</h1>
